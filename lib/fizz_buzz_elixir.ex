dfa65825fe97c016c7b64d5f9081e71518c50459
Jim Kirkbride <james.kirkbride@gmail.com>
2018-12-15 15:58:06 -0500

    Handling all other numbers.

--- lib/fizz_buzz_elixir.ex
+++ lib/fizz_buzz_elixir.ex
@@ -5,6 +5,7 @@ defmodule FizzBuzz do
       rem(val, 15) == 0 -> "fizz buzz"
       rem(val, 3) == 0 -> "fizz"
       rem(val, 5) == 0 -> "buzz"
+      true -> to_string(val)
     end
   end
 end
--- test/fizz_buzz_elixir_test.exs
+++ test/fizz_buzz_elixir_test.exs
@@ -15,5 +15,9 @@ defmodule FizzBuzzTest do
     test "it returns \"fizz buzz\" for numbers divisible by 3 and 5" do
       assert FizzBuzz.evaluate(15) == "fizz buzz"
     end
+
+    test "it returns the number back for any other number" do
+      assert FizzBuzz.evaluate(2) == "2"
+    end
   end
 end
